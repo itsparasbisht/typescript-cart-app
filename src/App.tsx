1ee79698c424ea132ba82c9b121e3f17c1ad35dc
itsparasbisht <iamparasbisht@gmail.com>
2023-02-18 18:45:29 +0530

    fresh start

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,9 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <Container className="my-4">
-      <Routes>
-        <Route path="/" element={<h1>hi</h1>} />
-        <Route path="/new" element={<h1>new</h1>} />
-        <Route path="/:id">
-          <Route index element={<h1>Show</h1>} />
-          <Route path="edit" element={<h1>Edit</h1>} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Container>
+    <>
+      <h1>hello</h1>
+    </>
   );
 }
 
