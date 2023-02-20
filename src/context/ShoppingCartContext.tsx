27170933aabceee6057c62d1afc53f8efd7732a1
itsparasbisht <iamparasbisht@gmail.com>
2023-02-20 19:58:13 +0530

    shopping cart context

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,19 @@
+import { createContext, useContext } from "react";
+
+type ShoppingCartProviderProps = {
+  children: React.ReactNode;
+};
+
+const ShoppingCartContext = createContext({});
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  return (
+    <ShoppingCartContext.Provider value={{}}>
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
